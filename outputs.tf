17049ed37f93bc9bdc0d49034b4fb60745f841e2
Derek Young <derek.j.n.young@gmail.com>
2023-09-13 20:44:59 -0700

    Output IP addresses of instances

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
 output "instance_uuids" {
-  value = tolist(openstack_compute_instance_v2.os_instances.*.id)
+  value = openstack_compute_instance_v2.os_instances.*.id
+}
+
+output "ip_addresses" {
+  value = openstack_networking_floatingip_v2.os_floatingips.*.address
+  description = "Public IP addresses"
 }
